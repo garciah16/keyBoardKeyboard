d43c0b35460a25908a5fa53d549cdecdd586c23e
garciah16 <garciah16@up.edu>
2016-03-03 19:55:49 -0800

    keyboard interface
    the keyboard has been interfaced with the due. thanks to jared and
    connor!

--- keyBoard/keyBoard.ino
+++ keyBoard/keyBoard.ino
@@ -1,17 +1,16 @@
 #include "waves.h"
 
-int key[] = {46, 44, 42, 40, 38, 36, 34, 32, 30};
+//int key[] = {30, 32, 34, 36, 38, 40, 42, 44, 46};
+int key[] = {30, 44, 42, 40, 38, 36, 34, 32, 46};
 int scanOut[] = {22, 24};
 int freq[] = {221, 208, 196, 185, 174, 164, 155, 146, 138, 130, 122, 115, 109};
 
 void setup() {
-  for(int i = 0; i <13; i++)
+  Serial.begin(9600);
+  for(int i = 0; i <9; i++)
   {
-    pinMode(key[i], INPUT);
+    pinMode(key[i], INPUT_PULLUP);
   }
-  pinMode(scanOut[0], OUTPUT);
-  pinMode(scanOut[1], OUTPUT);
-  pinMode(26, OUTPUT);
   analogWriteResolution(12);
   analogReadResolution(12);
 }
@@ -19,29 +18,19 @@ void setup() {
 void loop() {
 
    
-  while(true) //testing digital pins. why won't they work????
-  {
-    digitalWrite(26, LOW);
-    delay(100);
-    digitalWrite(26, HIGH);
-    delay(100);
-//   while(key[8] == HIGH)
-//   {
-//    playKey(221);
-//   }
-  }
-  
-  //go through our array of buttons
- digitalWrite(scanOut[0], HIGH);
- digitalWrite(scanOut[1], LOW);
+//go through our array of buttons
+ delay(5);
+ pinMode(scanOut[0], OUTPUT);
+ pinMode(scanOut[1], INPUT_PULLUP);
+ digitalWrite(scanOut[0], LOW);
  //scan through the keys and find one that is pressed
  for(int i = 1; i < 9; i++)
  {
-   while(key[i] == HIGH)
+   while(digitalRead(key[i]) == LOW)
    {
       if(i == 8)
       {
-        playKey[0]; //special case where the wires don't match
+        playKey(0); //special case where the wires don't match
       }
       else
       {
@@ -49,24 +38,26 @@ void loop() {
       }
    }
  }
- digitalWrite(scanOut[0], LOW);
- digitalWrite(scanOut[1], HIGH);
+ delay(5);
+ pinMode(scanOut[0], INPUT_PULLUP);
+ pinMode(scanOut[1], OUTPUT);
+ digitalWrite(scanOut[1], LOW);
  for(int j = 0; j < 5; j++)
  {
-   while(key[j] == HIGH)
+   while(digitalRead(key[j]) == LOW)
    {
     playKey(j+8);
    }
+  
  }
- 
-}
+}//end loop
 
 
-void playKey(float delayTime){
+void playKey(int keyNum){
   int j = 0;
   for(int i=0; i<271; i++) { //
     analogWrite(DAC1, bassTable[i]);//sin table
-    delayMicroseconds(delayTime); //determines the frequency of the sine wav
+    delayMicroseconds(freq[keyNum]); //determines the frequency of the sine wav
     i+=3;
   }
 }
