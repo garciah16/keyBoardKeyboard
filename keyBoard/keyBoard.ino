925fad3685eac9a63fe963063a9f563865921028
garciah16 <garciah16@up.edu>
2016-03-15 20:32:56 -0700

    tremolo with octave
    fixed the discrepancy between freqs and tremolo rate. added a brand new
    lower octave.

--- keyBoard/keyBoard.ino
+++ keyBoard/keyBoard.ino
@@ -5,6 +5,14 @@
 int key[] = {30, 44, 42, 40, 38, 36, 34, 32, 46};
 int scanOut[] = {22, 24};
 int freq[] = {221, 208, 196, 185, 174, 164, 155, 146, 138, 130, 122, 115, 109};
+int freq2[] = {444,419,395,373,352,333,313,295,279,263,248,234, 221};
+
+
+int tremolo = 49;
+boolean tremOn = false;
+
+int octaver = 51;
+boolean octaveDown = false;
 
 void setup() {
   Serial.begin(9600);
@@ -14,10 +22,12 @@ void setup() {
   }
   analogWriteResolution(12);
   analogReadResolution(12);
+  attachInterrupt(digitalPinToInterrupt(tremolo), tremoloISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(octaver), octaveISR, RISING);
 }
 
 void loop() {
-
+ 
    
 //go through our array of buttons
  delay(5);
@@ -59,16 +69,44 @@ void playKey(int freqNum, int keyNum){
   for (int j = 0; j< 499; j++){ 
 
     for(int i=0; i<271; i++) { //
-      analogWrite(DAC1, (int)(lfo[j]*bassTable[i]));//sin table
-      delayMicroseconds(freq[freqNum]); //determines the frequency of the sine wav
+      //multiplying my lfo creates the effect
+      if(tremOn){
+        analogWrite(DAC1, (int)(lfo[j]*bassTable[i]));//sin table
+      }
+      else{
+        analogWrite(DAC1, (bassTable[i]));//sin table
+      }
+      
+      if(octaveDown){
+        delayMicroseconds(freq2[freqNum]); //determines the frequency of the sine wav
+      }
+      else
+      {
+        delayMicroseconds(freq[freqNum]);
+      }
       i+=3;
     }
     if(digitalRead(key[keyNum]) != LOW) //don't continue note while key is not pressed
     {
       break;
     }
-    j+=10;
+    if(octaveDown){
+      j = j + (freq2[freqNum] / 10);
+    }
+    else{
+      j = j + (freq[freqNum] / 10);
+    }
   }
+}
+
+void tremoloISR()
+{
+  tremOn = !tremOn;
+}
+
+void octaveISR()
+{
+  octaveDown = !octaveDown;
 }
 
 
